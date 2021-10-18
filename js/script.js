56ab684c941b3b0bad4e4a62363ed95df9feca83
CarlosStrider120 <sherifinho.augusto@gmail.com>
2021-10-17 21:13:09 -0300

    Verificando quando uma celula foi clicada e criando os turnos dos jogadores e fazendo passar de turno depois que alguém joga

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const celulas = document.querySelectorAll(".celula");
+let checarTurno = true;
+const jogadorX = "X";
+const jogadorO = "O";
+
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".celula")) {
+        jogar(event.target.id);
+    }
+});
+
+function jogar(id) {
+    const celula = document.getElementById(id);
+    turno = checarTurno ? jogadorX : jogadorO;
+    celula.textContent = turno;
+    checarTurno = !checarTurno;
+    checarVenceador(turno);
+}
+
+function checarVencerdor(turno) {
+
+}
